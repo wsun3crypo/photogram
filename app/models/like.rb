00f313f2ba370374fa957c936dc37a54454ee9d2
anonymous anonymous <wsun3@uchicago.edu>
2021-09-06 07:27:31 +0000

    Generate likes resource with ideas:scaffold

---
+++ app/models/like.rb
@@ -0,0 +1,14 @@
+class Like < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+  # Scopes
+
+  def to_s
+    photo.to_s
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :likes
   resources :photos
   devise_for :users
   resources :users

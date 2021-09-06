3ec9c00e356a62fcec912e83b4896118014ce217
anonymous anonymous <wsun3@uchicago.edu>
2021-09-06 07:27:55 +0000

    Whitelist columns in Post for ActiveAdmin

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Post do
 
+ permit_params :body, :image, :users_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
